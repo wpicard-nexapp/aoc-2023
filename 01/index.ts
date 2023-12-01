ef1d484a2425aad1053646e4ef974ad106eaf3b1
Sanensi <william.picard@hotmail.com>
2023-12-01 18:18:59 -0500

    feat: day01 part1

--- 01/index.ts
+++ 01/index.ts
@@ -1,1 +1,31 @@
-console.log("Hello via Bun!");
+import { readFileSync } from "fs";
+import { argv } from "process";
+
+const fileName = argv[2];
+const data = readFileSync(fileName, { encoding: "utf-8" }).split("\n");
+
+let result = 0;
+
+for (const line of data) {
+  let first: string | undefined;
+  let last: string | undefined;
+
+  for (const char of line) {
+    if (isNumber(char)) {
+      if (first === undefined) {
+        first = char;
+      }
+      last = char;
+    }
+  }
+
+  if (first && last) {
+    result += Number.parseInt(first + last);
+  }
+}
+
+console.log(result);
+
+function isNumber(char: string) {
+  return !Number.isNaN(Number.parseInt(char));
+}
