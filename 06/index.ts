42cf2e23457467b40e8a1d3225981ec5ef24d2eb
Sanensi <william.picard@hotmail.com>
2023-12-11 19:51:31 -0500

    feat: day06 part01

--- 06/index.ts
+++ 06/index.ts
@@ -1,1 +1,33 @@
-console.log("Hello via Bun!");
+import { readFileSync } from "fs";
+import { argv } from "process";
+
+const fileName = argv[2];
+const lines = readFileSync(fileName, { encoding: "utf-8" }).split("\n");
+const times = parseNumbers(lines[0]);
+const distancesToBeat = parseNumbers(lines[1]);
+
+const f = (totalTime: number, waitTime: number) => waitTime * (totalTime - waitTime);
+
+let result = 1;
+times.forEach((totalTime, index) => {
+  const distanceToBeat = distancesToBeat[index];
+
+  let n = 0;
+  for (let waitTime = 0; waitTime <= totalTime; waitTime++) {
+    const distance = f(totalTime, waitTime);
+    if (distance > distanceToBeat) {
+      n++;
+    }
+  }
+
+  result *= n;
+});
+
+console.log(result);
+
+function parseNumbers(line: string) {
+  return line
+    .split(/\s+/)
+    .splice(1)
+    .map((n) => Number.parseInt(n));
+}
